78d26a2d10195fabe6cfb753be7d7324e8916b50
2557003864@qq.com <z8240087z>
2017-08-07 16:28:10 +0800

    Jnuit测试Demo

---
+++ app/src/main/java/com/example/kaifa/jnuittestdemo/Calculator.java
@@ -0,0 +1,24 @@
+package com.example.kaifa.jnuittestdemo;
+
+/**
+ * Created by zhb on 2017/8/7.
+ */
+
+public class Calculator {
+    public double sum(double a, double b) {
+        return a+b;
+    }
+
+    public double substract(double a, double b) {
+        return 0;
+    }
+
+    public double divide(double a, double b) {
+        return 0;
+    }
+
+    public double multiply(double a, double b) {
+        return 0;
+    }
+
+}
